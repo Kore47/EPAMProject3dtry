42534dcc3e82022712508278321f11ebe3255131
Kore47 <47914104+Kore47@users.noreply.github.com>
2019-03-02 15:29:11 +0200

    Update and rename codedeploy_agentinstall to codedeploy_agentinstall.sh

---
+++ scripts/codedeploy_agentinstall.sh
@@ -0,0 +1,7 @@
+yum install ruby
+yum install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+service codedeploy-agent start
